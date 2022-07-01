d3a279e1fe7cbff76bd3954c52d69406581ef5bf
SaBobic <fabrizioettori@gmail.com>
2022-07-01 12:30:53 +0200

    added cellsNumber

--- js/script.js
+++ js/script.js
@@ -1,23 +1,10 @@
-//* MILESTONE 2
-
-/*
-Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
-*/
-
-//* Procedimento step-by-step
-
-/*
-1. Dichiaro le variabili globali: play button, #box, numero di righe e colonne
-2. Creo una funzione che mi permetta di creare un numero ben definito di celle secondo il quantitativo di righe e colonne
-3. Uso il metodo addEeventListener per generate le celle al click del bottone play
-*/
-
 // Dichiaro le variabili globali
 const playButton = document.getElementById("play-button");
 const boxElement = document.getElementById("box");
 let cell;
 let boxRows;
 let boxCols;
+let cellsNumber;
 let difficultyChoice;
 
 // Creo una funzione che mi permetta di creare un div con classi cell e easy/medium/hard
@@ -25,7 +12,6 @@ const createCell = (content, level) => {
     const cell = document.createElement("div");
     cell.classList.add("cell", level);
 
-    //* MILESTONE 3
     // In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100
     cell.innerText = content;
 
@@ -51,13 +37,14 @@ playButton.addEventListener("click", function () {
             boxRows = boxCols = 7;
     }
 
+    cellsNumber = boxRows * boxCols;
+
     // Riempio il box con le nuove celle
-    for (let i = 1; i <= boxRows * boxCols; i++) {
+    for (let i = 1; i <= cellsNumber; i++) {
         cell = createCell(i, difficultyChoice);
 
         boxElement.appendChild(cell);
 
-        //* MILESTONE 4
         // Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
         cell.addEventListener("click", function () {
             this.classList.toggle("active");
